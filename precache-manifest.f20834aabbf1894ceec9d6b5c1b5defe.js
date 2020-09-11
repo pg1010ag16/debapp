4c66bcf5da246bed8e67f26a19a0c91faa04ea0e
pg1010ag16 <61314093+pg1010ag16@users.noreply.github.com>
2020-09-11 21:56:14 +0530

    Add files via upload

---
+++ precache-manifest.f20834aabbf1894ceec9d6b5c1b5defe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "360adb2d1a7c6f365e338090f065ed0c",
+    "url": "https://pg1010ag16.github.io/debapp/index.html"
+  },
+  {
+    "revision": "806350b9df07af62dffd",
+    "url": "https://pg1010ag16.github.io/debapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7defd9bfb7eab4e5d33f",
+    "url": "https://pg1010ag16.github.io/debapp/static/js/2.309576f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://pg1010ag16.github.io/debapp/static/js/2.309576f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "806350b9df07af62dffd",
+    "url": "https://pg1010ag16.github.io/debapp/static/js/main.dfa052ac.chunk.js"
+  },
+  {
+    "revision": "19217d4c13093ea11f9c",
+    "url": "https://pg1010ag16.github.io/debapp/static/js/runtime-main.e2eaf6a2.js"
+  }
+]);
